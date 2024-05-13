1b2a587ed83287e581e60e8a22cc91f51ac744b3
aman-ksolves <aman.ali@ksolves.com>
2024-05-13 23:22:50 +0530

    [ADD] Search functionality

--- custom_user/static/src/custom_menu_fn.js
+++ custom_user/static/src/custom_menu_fn.js
@@ -1,9 +1,13 @@
 /* @odoo-module */
-
+import { useState } from "@odoo/owl";
 import { patch } from "@web/core/utils/patch";
 import { SwitchCompanyMenu} from "@web/webclient/switch_company_menu/switch_company_menu";
 
 patch(SwitchCompanyMenu.prototype, {
+    setup() {
+        super.setup(...arguments);
+        this.inputValue = useState({ name : ""});
+      },
     selectAll(){
         for (let companyId in this.companyService.allowedCompanies){
             let comp = this.companyService.allowedCompanies[companyId];
@@ -13,5 +17,8 @@ patch(SwitchCompanyMenu.prototype, {
     },
     deselectAll(){
         this.companySelector.switchCompany("loginto",this.companyService.currentCompany.id)
+    },
+    onKeyUpFunction(e){
+        this.inputValue.name = e.target.value;
     }
 });
